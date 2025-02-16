ca6ed679a704971ef867719a020152c426213fee
DawitMac <tigistugaraneh5@gmail.com>
2025-02-16 18:29:12 +0300

    importing issue fixed

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
-import WelcomeMessage from './WelcomeMessage
+import WelcomeMessage from './components/WelcomeMessage'
+
 
 function App() {
   return (
