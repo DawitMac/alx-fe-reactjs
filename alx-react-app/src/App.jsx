2b32b1fe093179b1d366afc873262fa7cd6fe306
DawitMac <tigistugaraneh5@gmail.com>
2025-02-16 19:24:16 +0300

    header and footer created

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 
 function App() {
   return (
     <>
        <WelcomeMessage />
+       <Header />
+       <MainContent />
+       <Footer />
     </>
   )
 }
