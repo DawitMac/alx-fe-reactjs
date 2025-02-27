349a87a73aca70fcc56193c1074bae4fdfaab7cd
DawitMac <tigistugaraneh5@gmail.com>
2025-02-27 17:09:07 +0300

    alx-react-app-new folder added to the project

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <main style={{ backgroundColor: 'lightgray', padding: '10px', margin: '10px' }}>
+     <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+
+  )
+}
+
+export default MainContent
