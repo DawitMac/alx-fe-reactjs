c257dd80921f4afe26116acc8c93e76f00611c10
DawitMac <tigistugaraneh5@gmail.com>
2025-03-02 22:44:20 +0300

    importation adjusted in app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './App.css'
-import { AddRecipeForm } from './components/AddRecipeForm'
-import { RecipeList } from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
+
 
 const App = () => {
   return (
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,6 +1,6 @@
 import { useRecipeStore } from './recipeStore';
 
-  export const RecipeList = () => {
+  const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
 
     return (
@@ -14,3 +14,5 @@ import { useRecipeStore } from './recipeStore';
       </div>
     );
   };
+
+  export default RecipeList;
