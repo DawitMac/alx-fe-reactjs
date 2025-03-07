07b848f3f812ecb35cf7aa54c2831a3a1dd8a3c9
DawitMac <tigistugaraneh5@gmail.com>
2025-03-07 20:27:16 +0300

    link added to the RecipeList file

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import useRecipeStore from "./recipeStore";
 
 import RecipeDetails from "./RecipeDetails";
+import { Link } from "react-router-dom";
 
   const RecipeList = () => {
     const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
   console.log("filteredRecipes", filteredRecipes , typeof(filteredRecipes))
     return (
-      <div>
+      <Link to="/add-recipe">
         {filteredRecipes.map(recipe => (
           <RecipeDetails key={recipe.id} recipeId={recipe.id} />
         ))}
 
-      </div>
+      </Link>
     );
   };
 
