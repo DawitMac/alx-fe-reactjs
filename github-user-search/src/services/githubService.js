53a79f000962e753c15522d85746b5df2c16d983
DawitMac <tigistugaraneh5@gmail.com>
2025-04-05 10:30:14 +0300

    url modified at githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,18 +1,25 @@
 import axios from "axios";
+
 const API_URL = 'https://api.github.com';
 
 export const fetchUserData = async ({ username, location, minRepos }) => {
   const query = [];
+  
   if (username) query.push(`user:${username}`);
   if (location) query.push(`location:${location}`);
   if (minRepos) query.push(`repos:>${minRepos}`);
+  
   const queryString = query.join(' ');
 
-  const response = await axios.get(`${API_URL}/search/users?q=${encodeURIComponent(queryString)}`);
-  if (!response.ok) {
-    throw new Error(`Error: ${response.statusText}`);
+  try {
+    const response = await axios.get(`${API_URL}/search/users`, {
+      params: {
+        q: queryString,
+      },
+    });
+
+    return response.data; // Return the user data from the response
+  } catch (error) {
+    throw new Error(`Error: ${error.response ? error.response.data.message : error.message}`);
   }
-  return await response.json();
 };
-    
-    
